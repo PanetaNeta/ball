9ef5bb9b9498049da2b657e03fa7c6f651bd99b7
PanetaNeta <charm.pntnt@gmail.com>
2024-05-22 03:57:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 0 },
+      gravity: { y: 300 },
       debug: false
     }
   },
@@ -18,34 +18,67 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let timerText;
+let startTime;
+let balls;
+
 function preload() {
   this.load.image('background', 'assets/background.png');
   this.load.image('ball', 'assets/ball.png');
 }
 
 function create() {
-  this.add.image(400, 300, 'background');
+  this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background').setDisplaySize(window.innerWidth, window.innerHeight);
   
-  const balls = this.physics.add.group({
+  balls = this.physics.add.group({
     key: 'ball',
-    repeat: 39,
-    setXY: { x: 12, y: 12, stepX: 70 }
+    repeat: 19,
+    setXY: { x: Phaser.Math.Between(50, window.innerWidth - 50), y: 0, stepX: 70 }
   });
-
-  Phaser.Actions.ScaleXY(balls.getChildren(), -0.5, -0.5);
 
   balls.children.iterate(function (ball) {
     ball.setInteractive();
-    ball.on('pointerdown', function () {
-      ball.setTint(0xff0000);
+    ball.setBounce(0.6);
+    ball.setCollideWorldBounds(true);
+    ball.setScale(Phaser.Math.FloatBetween(0.5, 1.5));
+    ball.on('pointerdown', function (pointer) {
+      this.setTint(0xff0000);
+      this.input.dragStartX = this.x;
+      this.input.dragStartY = this.y;
+      this.scene.input.setDraggable(this);
+    });
+
+    ball.on('drag', function (pointer, dragX, dragY) {
+      this.x = dragX;
+      this.y = dragY;
+    });
+
+    ball.on('pointerup', function () {
+      this.clearTint();
+      this.scene.input.setDraggable(this, false);
     });
   });
 
-  this.input.on('gameobjectdown', function (pointer, gameObject) {
-    gameObject.destroy();
-  }, this);
+  this.input.on('dragend', function (pointer, gameObject) {
+    gameObject.clearTint();
+    gameObject.body.moves = true;
+  });
+
+  startTime = new Date();
+  timerText = this.add.text(16, 16, 'Time: 0s', { fontSize: '32px', fill: '#FFF' });
+  this.time.addEvent({
+    delay: 1000,
+    callback: updateTimer,
+    callbackScope: this,
+    loop: true
+  });
+
+  this.physics.add.collider(balls, balls);
 }
 
-function update() {
-  // อัพเดตเกมในแต่ละเฟรม
+function update() {}
+
+function updateTimer() {
+  const elapsed = Math.floor((new Date() - startTime) / 1000);
+  timerText.setText('Time: ' + elapsed + 's');
 }
